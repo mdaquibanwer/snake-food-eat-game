3da49c1c27f8d16e061a45a12fec8581a1ff9ff2
Aquib <mdaquibanwer@gmail.com>
2022-12-07 20:01:14 +0530

    basic game complete some bug to fix

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let direction = {x:0,y:0};
+let inputDirection = {x:0,y:0};
 const eatFoodSound = new Audio("eatFood.wav");
 const changeDirectionSound = new Audio("changeDirection.mp3");
 const gameOverSound = new Audio("gameOver.wav");
 const backgroundSound = new Audio("bgGame.mp3");
 let gameBoard = document.getElementById('gameBoard')
+let score = 0;
 let lastpaintTime = 0;
 let speed = 2;
 let snakeArr = [
     {x:13,y:15}
 ]
-let food = {x:6,y:5};
+let a = 2;
+let b = 16;
+let food = {x : Math.round(a+ (b-a)*Math.random()), y : Math.round(a+ (b-a)*Math.random())};
 
 
 // -------------------------- game Functions ---------------------------
@@ -21,11 +24,42 @@ function main(cTime){
         return;
     }
     lastpaintTime = cTime;
-    // gameEngine();
+    gameEngine();
 }
 
 function gameEngine() {
     // 1. Update the snake array and food
+
+    function isCollide(snakeArr) {
+        return false;
+    }
+
+    if(isCollide(snakeArr)){
+        backgroundSound.pause();
+        gameOverSound.play();
+        inputDirection = {x:0,y:0}
+        alert("GameOver Press Any key to Start Again")
+        snakeArr = [{x:13,y:15}];
+        backgroundSound.play();
+        score = 0;
+    }
+
+    // if food is eaten , then increment the score and regenerate the food
+
+    if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
+        eatFoodSound.play();
+        snakeArr.unshift({x: snakeArr[0].x + inputDirection.x, y: snakeArr[0].y + inputDirection.y})
+        food = {x : Math.round(a+ (b-a)*Math.random()), y : Math.round(a+ (b-a)*Math.random())}
+    }
+
+    // Moving the Snake 
+    for (let i = snakeArr.length-2 ; i >=0; i--) {
+        // const element = snakeArr[i];
+        snakeArr[i+1] = {...snakeArr[i]};
+    }
+
+    snakeArr[0].x += inputDirection.x;
+    snakeArr[0].y += inputDirection.y;
 
     // 2. Display the snake and food
 
@@ -35,9 +69,11 @@ function gameEngine() {
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
-        snakeElement.classList.add('snake')
         if(index === 0){
             snakeElement.classList.add('head')
+        }
+        else{
+           snakeElement.classList.add('snake')
         }
         gameBoard.appendChild(snakeElement);
     })
@@ -51,12 +87,38 @@ function gameEngine() {
 }
 
 
-
-
-
-
-
-
 // ----------------------------- game Logic ------------------------------
 
 window.requestAnimationFrame(main)
+window.addEventListener('keydown',e=>{
+    inputDirection = {x:0,y:1}   // start the game
+    changeDirectionSound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("Arrow Up")  
+            inputDirection.x = 0;  
+            inputDirection.y = -1;  
+            break;
+
+        case "ArrowDown":
+            console.log("Arrow Down")
+            inputDirection.x = 0;  
+            inputDirection.y = 1;    
+            break;
+
+        case "ArrowLeft":
+            console.log("Arrow Left")    
+            inputDirection.x = -1;  
+            inputDirection.y = 0;
+            break;
+
+        case "ArrowRight":
+            console.log("Arrow Right")  
+            inputDirection.x = 1;  
+            inputDirection.y = 0;  
+            break;
+
+        default:
+            break;
+    }
+})
