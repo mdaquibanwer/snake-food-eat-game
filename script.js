13cef6b4b7b21493d923a5c1034e3210e841cbcc
Aquib <mdaquibanwer@gmail.com>
2022-12-07 21:37:25 +0530

    snake game complete

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const changeDirectionSound = new Audio("changeDirection.mp3");
 const gameOverSound = new Audio("gameOver.wav");
 const backgroundSound = new Audio("bgGame.mp3");
 let gameBoard = document.getElementById('gameBoard')
+let scoreBox = document.getElementById('score')
 let score = 0;
 let lastpaintTime = 0;
-let speed = 2;
+let speed = 4;
 let snakeArr = [
     {x:13,y:15}
 ]
@@ -30,31 +31,56 @@ function main(cTime){
 function gameEngine() {
     // 1. Update the snake array and food
 
-    function isCollide(snakeArr) {
-        return false;
+    function isCollide(snake) {
+        // if you collide with yourself
+        for (let i = 1; i < snakeArr.length; i++) {
+            if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+                return true;
+            }
+        }
+        // if you collide with boundry
+        if(snake[0].x>=18 || snake[0].x<=0 || snake[0].y>=18 || snake[0].y<=0){
+            return true
+        }
     }
 
     if(isCollide(snakeArr)){
-        backgroundSound.pause();
         gameOverSound.play();
+        backgroundSound.pause();
         inputDirection = {x:0,y:0}
         alert("GameOver Press Any key to Start Again")
+        score = 0;
         snakeArr = [{x:13,y:15}];
         backgroundSound.play();
-        score = 0;
+        
     }
 
     // if food is eaten , then increment the score and regenerate the food
 
     if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
         eatFoodSound.play();
+        score+=1;
+        if (score>=5 && score <=10) {
+            speed = 8
+        } else if (score>10 && score <=15){
+            speed = 12
+        } else if (score>15 && score <=20){
+            speed = 16
+        } else if (score>20){
+            speed = 20
+        }
+        if(score>highestScore){
+            highestScore = score;
+            localStorage.setItem(highScore,JSON.stringify(highestScore));
+            highScores.innerHTML = "Highest Score : " + highestScore;
+        }
+        scoreBox.innerHTML = "Score : " + score;
         snakeArr.unshift({x: snakeArr[0].x + inputDirection.x, y: snakeArr[0].y + inputDirection.y})
         food = {x : Math.round(a+ (b-a)*Math.random()), y : Math.round(a+ (b-a)*Math.random())}
     }
 
     // Moving the Snake 
     for (let i = snakeArr.length-2 ; i >=0; i--) {
-        // const element = snakeArr[i];
         snakeArr[i+1] = {...snakeArr[i]};
     }
 
@@ -91,29 +117,30 @@ function gameEngine() {
 
 window.requestAnimationFrame(main)
 window.addEventListener('keydown',e=>{
+    backgroundSound.play();
     inputDirection = {x:0,y:1}   // start the game
     changeDirectionSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("Arrow Up")  
+            // console.log("Arrow Up")  
             inputDirection.x = 0;  
             inputDirection.y = -1;  
             break;
 
         case "ArrowDown":
-            console.log("Arrow Down")
+            // console.log("Arrow Down")
             inputDirection.x = 0;  
             inputDirection.y = 1;    
             break;
 
         case "ArrowLeft":
-            console.log("Arrow Left")    
+            // console.log("Arrow Left")    
             inputDirection.x = -1;  
             inputDirection.y = 0;
             break;
 
         case "ArrowRight":
-            console.log("Arrow Right")  
+            // console.log("Arrow Right")  
             inputDirection.x = 1;  
             inputDirection.y = 0;  
             break;
@@ -122,3 +149,15 @@ window.addEventListener('keydown',e=>{
             break;
     }
 })
+
+// ------------------------- Store High Score In Local Storage ----------------------
+
+let highScore = localStorage.getItem("highScore");
+if(highScore === null){
+    let highestScore = 0
+    localStorage.setItem("highScore",JSON.stringify(highestScore))
+}
+else{
+    highestScore = JSON.parse(highScore)
+    highScores.innerHTML = "Highest Score : " + highScore;
+}
